6b60810eed434467b1fe976aba0a3aa9ee3b82fa
Hamza Basrai <hamza.basrai@gmail.com>
2020-12-21 14:01:27 -0500

    Increase horizontal padding on Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@ import Navbar from './navbar';
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="h-screen flex flex-col container mx-auto px-4 md:px-8">
+      <div className="h-screen flex flex-col container mx-auto px-4 md:px-10">
         <header className="mt-4 md:mt-8 py-4 sticky top-0 z-10 bg-blueGray-50">
           <Navbar />
         </header>
