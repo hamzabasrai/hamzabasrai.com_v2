224f735df17c7014f6e839c0cfba7a3f2d096722
Hamza Basrai <hamza.basrai@gmail.com>
2020-12-21 23:23:22 -0500

    Update footer styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,40 +5,33 @@ import LinkedIn from '../assets/linkedin.inline.svg';
 
 const Footer = () => {
   return (
-    <footer className="content-wrapper text-primary flex justify-between items-center my-2 lg:my-4">
-      <p>© {new Date().getFullYear()} Hamza Basrai</p>
-      <div className="space-x-4 flex justify-end">
-        {/* <span>
-          <a
-            href="mailto:hamza.basrai@gmail.com"
-            aria-label="Email hamza.basrai@gmail.com"
-            title="Email"
-          >
-            <Envelope className="h-8 w-8 transition duration-100" />
-          </a>
-        </span> */}
-        <span>
-          <a
-            href="https://github.com/hamzabasrai"
-            aria-label="Visit GitHub profile"
-            title="Visit GitHub profile"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <GitHub className="h-8 w-8 transition duration-100 fill-current" />
-          </a>
-        </span>
-        <span>
-          <a
-            href="https://linkedin.com/in/hamzabasrai"
-            aria-label="Visit LinkedIn profile"
-            title="Visit LinkedIn profile"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <LinkedIn className="h-8 w-8 transition duration-100 fill-current" />
-          </a>
-        </span>
+    <footer className="text-primary bg-secondary py-2 lg:py-4">
+      <div className="content-wrapper flex justify-between items-center">
+        <p>© {new Date().getFullYear()} Hamza Basrai</p>
+        <div className="space-x-3 md:space-x-4 flex justify-end">
+          <span>
+            <a
+              href="https://github.com/hamzabasrai"
+              aria-label="Visit GitHub profile"
+              title="Visit GitHub profile"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GitHub className="h-7 w-7 md:h-8 md:w-8 transition duration-100 opacity-75 hover:opacity-100 fill-current" />
+            </a>
+          </span>
+          <span>
+            <a
+              href="https://linkedin.com/in/hamzabasrai"
+              aria-label="Visit LinkedIn profile"
+              title="Visit LinkedIn profile"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedIn className="h-7 w-7 md:h-8 md:w-8 transition duration-100 opacity-75 hover:opacity-100 fill-current" />
+            </a>
+          </span>
+        </div>
       </div>
     </footer>
   );
