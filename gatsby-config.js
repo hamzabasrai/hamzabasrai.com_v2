7582aa5e9098a38c50ea38cec0be8c3469bef6fa
Hamza Basrai <hamza.basrai@gmail.com>
2020-12-23 00:45:10 -0500

    Add gatsby-transformer-json plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,14 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
